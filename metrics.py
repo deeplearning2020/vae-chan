d39da3e914c7b56cf9182cb6b394a23d1aeb9560
goyeahia <yeahia.ruet@gmail.com>
2020-04-13 22:29:18 +0600

    pls

---
+++ metrics.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+
+def psnr(x, y, m_val = 255):
+    return tf.image.psnr(a = x, b = y, max_val = m_val)
+
+
+def ssim(x, y, m_val = 255):
+    return tf.image.ssim(img1 = x, img2 = y, max_val = m_val)
+
+
+def mse(x, y):
+    return tf.metrics.mean_squared_error(labels = x, predictions = y)
