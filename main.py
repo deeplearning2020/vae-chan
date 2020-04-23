8fb2a5a05c5edbbb804f84ef760628c09f4265fd
goyeahia <yeahia.ruet@gmail.com>
2020-04-23 13:49:02 +0600

    scene

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def main():
     es = EarlyStopping(monitor = 'loss', mode = 'min', verbose = 1, patience = 500) ## early stopping to prevent overfitting
 
     history = bvae.ae.fit(lr_image, hr_image,
-                epochs = 2000,
+                epochs = 1500,
                 batch_size = batchSize, callbacks = [es])
 
     """ reconstrucing high-resolution image from the low-resolution image """
