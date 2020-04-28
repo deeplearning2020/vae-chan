5b6f897b44ec9cf653603be531baddd0b174dd66
goyeahia <yeahia.ruet@gmail.com>
2020-04-28 12:33:04 +0600

    add vae

--- reconstructed-images/downsample.py
+++ reconstructed-images/downsample.py
@@ -8,14 +8,14 @@ def prepare_images(path, factor):
         img = cv2.imread(path + '/' + file)
         print(img.shape)
         h, w, _ = img.shape
-        new_height = h / factor
-        new_width = w / factor
-        img = cv2.resize(img, (int(new_width), int(new_height)), interpolation = cv2.INTER_CUBIC)
+        new_height = 256
+        new_width = 256
+        img = cv2.resize(img, (int(new_width), int(new_height)), interpolation = cv2.INTER_LINEAR)
         #img = cv2.resize(img, (w, h), interpolation = cv2.INTER_LINEAR)
         print('Saving {}'.format(file))
         cv2.imwrite((write_path + file), img)
 
-new_path = os.path.join(os.getcwd(),'hr_image')
-write_path = os.path.join(os.getcwd(), 'lr_image/')
+new_path = os.path.join(os.getcwd(),'final generation')
+write_path = os.path.join(os.getcwd(), 'final generation/')
 print(write_path)
 prepare_images(new_path, 3)
